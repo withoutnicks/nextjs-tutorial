8ea72151ce028a84e0aec7ce607431a74aa7379a
Gerardo Caycho <legalmentepother@gmail.com>
2024-10-26 11:31:03 -0500

    🌐 - Metatags

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,14 @@
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { space } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
 
 export default async function Page() {
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,22 @@
 import '@/app/ui/global.css';
 import { space } from '@/app/ui/fonts'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({children,}: {children: React.ReactNode;}) {
 	return (
 		<html lang='en'>
       <head>
         <title>NextJS - Dashboard Tutorial</title>
-        <link rel='icon' href='/icon-page.svg' />
+        <link rel='icon' href='/favicon.svg' />
       </head>
 			<body className={`${space.className} antialiased`}>{children}</body>
 		</html>
